10f10962253d164c66e4b473b1e780a2a0ca2a17
bnu123 <kash.bhanu7@gmail.com>
2019-05-01 20:08:58 +0530

    Phase One Started

--- install.sh
+++ install.sh
@@ -1,2 +1,31 @@
-# setting up usr-commands
+#:Updation and upgradation
+sudo apt-get -y update
+sudo apt-get -y upgrade
+
+#:Set up Softwares(apps) that you use.
+sudo apt-get -y install zsh
+sudo apt-get -y install curl
+sudo apt-get -y install wget
+curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
+sudo apt-get -y install vlc
+
+#Setting up zshrc,bashrc,vimrc,
+
+source_path="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
+
+
+# TODO:setting up usr-commands
+
+
+# TODO:Set up wallpaper, theme (download and apply it).
+
+
+
+
+
+
+
+
+
+
 
