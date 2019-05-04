069464b6a64a15a3d3228760121a316936e74567
bnu123 <kash.bhanu7@gmail.com>
2019-05-04 12:50:52 +0530

    Added functional abstraction

--- install.sh
+++ install.sh
@@ -1,35 +1,33 @@
 # absolute path of this script
 source_path="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
 
-#:Updation and upgradation
-echo "Welcome, Upgrading and Updating your System ..."
-sudo apt-get update
-sudo apt-get upgrade
+app_install() {
+    #:Updation and upgradation
+    echo "Welcome, Upgrading and Updating your System ..."
+    sudo apt-get update
+    sudo apt-get upgrade
 
-echo "Frequent Use App Installation"
-echo "Installing zsh"
-sudo apt-get -y install zsh
-echo "Installing curl"
-sudo apt-get -y install curl
-echo "Installing wget"
-sudo apt-get -y install wget
-echo "Installing oh-my-zsh"
-curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
-echo "Installing vlc"
-sudo apt-get -y install vlc
-echo "Installing build-essential"
-sudo apt-get -y install build-essential
-echo "Installing gcc, g++ compilers"
-sudo apt-get -y install gcc
-sudo apt-get -y install g++
-echo "Installing synaptic"
-sudo apt-get -y install synaptic
-echo "Setting up zshrc, bashrc and vimrc "
-mv .zshrc ~/.zshrc.old
-mv .bashrc ~/.bashrc.old
-ln -s $source_path/.zshrc ~/
-ln -s $source_path/.bashrc ~/
-echo "Note : Old configuration in ~/.zshrc.old and ~/.bashrc.old"
+    #Frequently used apps
+    local apps=('zsh' 'curl' 'wget' 'vlc' 'build-essential' 'gcc' 'g++' 'synaptic' 'gnome-tweaks')
+
+    for i in "${apps[@]}"; do
+        echo "Installing $i"
+        sudo apt-get -y install "$i"
+    done
+
+    #installing oh-my-zsh
+    curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
+
+    echo "Setting up zshrc, bashrc and vimrc "
+    mv .zshrc ~/.zshrc.old
+    mv .bashrc ~/.bashrc.old
+    ln -s $source_path/.zshrc ~/
+    ln -s $source_path/.bashrc ~/
+    echo "Note : Old configuration in ~/.zshrc.old and ~/.bashrc.old"
+}
+
+#invoke install function
+app_install
 
 #:setting up usr-commands
 echo "User commands setup"
