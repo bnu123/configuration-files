3e2dfd995b627fb1959c76ce2000f391f8426c25
bnu123 <kash.bhanu7@gmail.com>
2019-05-06 17:13:54 +0530

    Installation For Vscode/Functional Abstraction

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ app_install() {
     sudo apt-get upgrade
 
     #Frequently used apps
-    local apps=('zsh' 'curl' 'wget' 'vlc' 'build-essential' 'gcc' 'g++' 'synaptic' 'gnome-tweaks')
+    local apps=('zsh' 'curl' 'wget' 'vlc' 'build-essential' 'gcc' 'g++' 'synaptic' 'gnome-tweaks' 'software-properties-common' 'apt-transport-https')
 
     for i in "${apps[@]}"; do
         echo "Installing $i"
@@ -27,16 +27,40 @@ app_install() {
     echo "Note : Old configuration in ~/.zshrc.old and ~/.bashrc.old"
 }
 
-#invoke install function
-app_install
+configure_editor() {
+    echo "Sorry Not Implemented Yet"
+}
 
-#:setting up usr-commands
-echo "User commands setup"
-ln -s $source_path/usr-commands/crnch /usr/local/bin/crnch
-ln -s $source_path/usr-commands/fastcpp /usr/local/bin/fastcpp
-ln -s $source_path/usr-commands/hub /usr/local/bin/hub
+dev_configure() {
+#Editor==============================
+    echo "Setting up Development environment"
 
-# TODO:Set up wallpaper, theme (download and apply it).
+    #installing vscode
+    echo "Downloading vscode"
 
-    # TODO: Set up vscode.
+    #importing Microsoft GPG key
+    wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 
+    #enable VsCode Repo
+    sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+    sudo apt-get -y install code
+
+    #configure_editor with extensions and editor settings
+    conifgure_editor
+}
+
+usr_commands(){
+    #:setting up usr-commands
+    echo "User commands setup"
+    ln -s $source_path/usr-commands/crnch /usr/local/bin/crnch
+    ln -s $source_path/usr-commands/fastcpp /usr/local/bin/fastcpp
+    ln -s $source_path/usr-commands/hub /usr/local/bin/hub
+}
+
+env_configure(){
+
+}
+
+app_install #install the apps
+dev_configure #Configure your development environment
+env_configre #Configure environment (Fonts,Themes etc)
